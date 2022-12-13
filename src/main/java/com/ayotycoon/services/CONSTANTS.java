49a0ad51d34088da0e4e8b0de5cc190e3c789a94
ayotycoon <kaysehventeen@gmail.com>
2022-12-12 23:18:23 -0600

    Enabled redis multiservice communication

--- src/main/java/com/ayotycoon/services/CONSTANTS.java
+++ src/main/java/com/ayotycoon/services/CONSTANTS.java
@@ -2,7 +2,6 @@ package com.ayotycoon.services;
 
 import com.ayotycoon.services.WSManager.WSManager;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Value;
 
 
 public class CONSTANTS {
